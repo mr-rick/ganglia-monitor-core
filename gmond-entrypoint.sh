e9091b5fea1bcb76b1edc4ae3dafe2c8c3cfe4c1
Rick Garcia <rgarcia@Ricks-iMac.local>
2018-05-11 18:26:39 -0700

    we need an init container for some reason, we want a dedicated namespace, and we now use labels to get the kubernetes service

--- gmond-entrypoint.sh
+++ gmond-entrypoint.sh
@@ -3,10 +3,14 @@
 export GANGLIA_NODE_ROLE=$(kubectl get node $KUBERNETES_NODENAME -o jsonpath='{.metadata.labels.ganglia/role}')
 [[ -z $GANGLIA_NODE_ROLE  ]] && GANGLIA_NODE_ROLE="NULL"
 
-export GANGLIA_GMOND_KUBERNETES_SERVICE=$(kubectl get service -l ganglia/role=$GANGLIA_NODE_ROLE | grep -v NAME | cut -d ' ' -f1 | head -n1)
-[[ -z $GANGLIA_GMOND_KUBERNETES_SERVICE  ]] && GANGLIA_GMOND_KUBERNETES_SERVICE="NULL"
+export GANGLIA_CLUSTER_NAME=$KUBERNETES_NODENAME
+if [[ ! $GANGLIA_NODE_ROLE = *"NULL"* ]]; then
+  GANGLIA_CLUSTER_NAME=$GANGLIA_NODE_ROLE
+fi
 
-export GANGLIA_GMOND_UNICAST_HOST=$(kubectl describe service $GANGLIA_GMOND_KUBERNETES_SERVICE | grep Endpoints | head -n1 | awk '{print $2}' | tr ',' '\n' | sort -n | cut -d ':' -f1)
+kubectl label pod $KUBERNETES_POD_NAME ganglia/role=$GANGLIA_NODE_ROLE
+
+export GANGLIA_GMOND_UNICAST_HOST=$(kubectl describe service -l ganglia/role=$GANGLIA_NODE_ROLE | grep Endpoints | head -n1 | awk '{print $2}' | tr ',' '\n' | sort -n | cut -d ':' -f1)
 [[ -z $GANGLIA_GMOND_UNICAST_HOST  ]] && GANGLIA_GMOND_UNICAST_HOST=localhost
 
 echo "GANGLIA_GMOND_UNICAST_HOST=$GANGLIA_GMOND_UNICAST_HOST"
